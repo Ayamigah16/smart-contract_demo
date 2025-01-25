b1703d967cae11a3b6d13ce2e3ea210f7aefad4e
Abraham Ayamigah <abkc070@gmail.com>
2025-01-25 16:20:16 +0000

    sample compile script

---
+++ compile.js
@@ -0,0 +1,11 @@
+const path =  require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+// get contract source
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+
+// display compile info
+console.log(solc.compile(source,1));
