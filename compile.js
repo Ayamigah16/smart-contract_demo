2079ccdf5adcca03897c6fae5b01138bef43abfc
Abraham Ayamigah <abkc070@gmail.com>
2025-01-27 22:14:51 +0000

    compile scripts

--- compile.js
+++ compile.js
@@ -7,5 +7,8 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf-8');
 
 
+// export compile info
+module.exports = solc.compile(source, 1).contracts[':Inbox']
+
 // display compile info
-console.log(solc.compile(source,1));
+// console.log(solc.compile(source,1));
