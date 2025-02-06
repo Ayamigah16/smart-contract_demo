42d98f2bdbce5375ad46249ba08137d402fbf871
Abraham Ayamigah <abkc070@gmail.com>
2025-02-06 15:31:52 +0000

    test setMessage function

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,6 +9,7 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+let INITIAL_MESSAGE = 'Hi there!';
 
 beforeEach(async() => {
     // Get a list of all accounts
@@ -16,14 +17,24 @@ beforeEach(async() => {
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts);
-        console.log(inbox);
-        // assert.ok(inbox.options.address);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async() => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, INITIAL_MESSAGE);
+    });
+
+    it('can change the message', async() => {
+        const newMessage = 'Bye there!';
+        await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, newMessage);
     });
 });
